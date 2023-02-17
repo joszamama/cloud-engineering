cee811aa2d79c38c97b9b5fd1b5304843b558a60
Mariano Manuel Torrado Sánchez <marianomtorrados@gmail.com>
2023-02-17 21:39:02 +0100

    feat: Added static methods for dashboard metrics in Trip and Application

--- server/models/Application.js
+++ server/models/Application.js
@@ -1,14 +1,55 @@
 import mongoose from "mongoose";
 
 const ApplicationSchema = new mongoose.Schema({
-    status: {type: String, enum:["PENDING", "REJECTED", "DUE", "ACCEPTED", "CANCELLED"], default: "PENDING"},
-    rejectReason: {type: String},
-    rejectComment: {type: String},
-    actor: {type: mongoose.Schema.Types.ObjectId, ref: 'Actor'},
-    trip: {type: mongoose.Schema.Types.ObjectId, ref: 'Trip'}
-}, {timestamps: true});
+    status: { type: String, enum: ["PENDING", "REJECTED", "DUE", "ACCEPTED", "CANCELLED"], default: "PENDING" },
+    rejectReason: { type: String },
+    rejectComment: { type: String },
+    actor: { type: mongoose.Schema.Types.ObjectId, ref: 'Actor' },
+    trip: { type: mongoose.Schema.Types.ObjectId, ref: 'Trip' },
+    statics: {
+        getRatiosByStatus: function () {
+            return this.aggregate([
+                {
+                    $facet: {
+                        totalCount: [
+                            {
+                                $group: {
+                                    _id: 0,
+                                    count: {
+                                        $count: {},
+                                    },
+                                },
+                            },
+                        ],
+                        groupCount: [
+                            {
+                                $group: {
+                                    _id: "$status",
+                                    count: { $count: {} },
+                                },
+                            },
+                        ],
+                    },
+                },
+                {
+                    $project: {
+                        ids: "$groupCount._id",
+                        ratios: {
+                            $map: {
+                                input: "$groupCount.count",
+                                as: "group",
+                                in: { $divide: ["$$group", { $first: "$totalCount.count" }] }
+                            },
+                        },
+                    },
+                },
+            ]);
+        }
+    }
 
-ApplicationSchema.methods.cleanup = function() {
+}, { timestamps: true });
+
+ApplicationSchema.methods.cleanup = function () {
     return {
         id: this._id,
         status: this.status,
