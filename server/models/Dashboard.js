b70da8cd40dd94d3e117149702af4c952e70e181
alesancor1 <alesancor1@alum.us.es>
2023-02-21 17:13:15 +0100

    feat: Implemented dashboard automatic computation

--- server/models/Dashboard.js
+++ server/models/Dashboard.js
@@ -1,4 +1,7 @@
 import mongoose from "mongoose";
+import Trip from "./Trip.js";
+import Finder from "./Finder.js";
+import Application from "./Application.js";
 
 const DashboardSchema = new mongoose.Schema({
     trip_average: { type: Number, required: true },
@@ -13,10 +16,12 @@ const DashboardSchema = new mongoose.Schema({
     trip_price_minium: { type: Number, required: true },
     trip_price_maximum: { type: Number, required: true },
     trip_price_deviation: { type: Number, required: true },
-    ratio_by_status: { type: Number, required: true }
+    ratio_by_status: { type: Object, required: true },
+    price_range_average: { type: Object, required: true },
+    top10_finder_keywords: { type: Array, required: true }
 }, {
     timestamps: true,
-    statics: { getApplicationMetrics, getTripMetrics, getPriceMetrics }
+    statics: { getApplicationMetrics, getTripMetrics, getFinderMetrics }
 });
 
 DashboardSchema.methods.cleanup = function() {
@@ -45,7 +50,7 @@ export default mongoose.model('Dashboard', DashboardSchema);
 
 /* Aggregation functions for dashboards */
 function getApplicationMetrics() {
-    return this.aggregate([
+    return Application.aggregate([
         {
             $facet: {
                 totalCount: [
@@ -84,85 +89,87 @@ function getApplicationMetrics() {
 }
 
 function getTripMetrics() {
-    return this.aggregate({
-        managedTrips: [
-            {
-                $group: {
-                    _id: "$manager",
-                    count: {
-                        $count: {},
+    return Trip.aggregate([{
+        $facet: {
+            managedTrips: [
+                {
+                    $group: {
+                        _id: "$manager",
+                        count: {
+                            $count: {},
+                        },
                     },
                 },
-            },
-            {
-                $group: {
-                    _id: 0,
-                    avg: {
-                        $avg: "$count",
-                    },
-                    min: {
-                        $min: "$count",
-                    },
-                    max: {
-                        $max: "$count",
-                    },
-                    std: {
-                        $stdDevSamp: "$count",
-                    },
-                },
-            },
-        ],
-        tripsApplications: [
-            {
-                $group: {
-                    _id: "$applications",
-                    count: {
-                        $count: {},
+                {
+                    $group: {
+                        _id: 0,
+                        avg: {
+                            $avg: "$count",
+                        },
+                        min: {
+                            $min: "$count",
+                        },
+                        max: {
+                            $max: "$count",
+                        },
+                        std: {
+                            $stdDevSamp: "$count",
+                        },
                     },
                 },
-            },
-            {
-                $group: {
-                    _id: 0,
-                    avg: {
-                        $avg: "$count",
-                    },
-                    min: {
-                        $min: "$count",
-                    },
-                    max: {
-                        $max: "$count",
-                    },
-                    std: {
-                        $stdDevSamp: "$count",
+            ],
+            tripsApplications: [
+                {
+                    $group: {
+                        _id: "$applications",
+                        count: {
+                            $count: {},
+                        },
                     },
                 },
-            },
-        ],
-        tripsPrice: [
-            {
-                $group: {
-                    _id: 0,
-                    avg: {
-                        $avg: "$price",
-                    },
-                    min: {
-                        $min: "$price",
-                    },
-                    max: {
-                        $max: "$price",
-                    },
-                    std: {
-                        $stdDevSamp: "$price",
+                {
+                    $group: {
+                        _id: 0,
+                        avg: {
+                            $avg: "$count",
+                        },
+                        min: {
+                            $min: "$count",
+                        },
+                        max: {
+                            $max: "$count",
+                        },
+                        std: {
+                            $stdDevSamp: "$count",
+                        },
                     },
                 },
-            },
-        ],
-    })
+            ],
+            tripsPrice: [
+                {
+                    $group: {
+                        _id: 0,
+                        avg: {
+                            $avg: "$price",
+                        },
+                        min: {
+                            $min: "$price",
+                        },
+                        max: {
+                            $max: "$price",
+                        },
+                        std: {
+                            $stdDevSamp: "$price",
+                        },
+                    },
+                },
+            ],
+        }
+    }])
 }
 
-function getPriceMetrics () {
-    return this.aggregate([
+function getFinderMetrics () {
+    return Finder.aggregate([
         {
             $facet: {
                 avgPriceStats: [
